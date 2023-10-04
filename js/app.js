46c7fcb2ccb77dfd9dd1fdeea8a810fce9673dd7
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-10-04 19:20:19 -0400

    added year feature

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 //Variables
 const resultado = document.querySelector("#resultado"); //Donde mostraremos los resultados, es un div en el archivo HTML
+const year = document.querySelector("#year");
+
+const max = new Date().getFullYear(); //Toma el año actual
+const min = max - 10;
+
+console.log(max);
+console.log(min);
 
 //Eventos
 document.addEventListener("DOMContentLoaded",  ()=>{
-    mostrarAutos();
+    mostrarAutos(); //Muestra los carros al cargar
+
+    //Llena las opciones de años
+    llenarSelect();
 });
 
 
@@ -12,10 +22,22 @@ function mostrarAutos() {
     
     autos.forEach( auto => {
         const {marca, modelo, year, precio, puertas, color, transmision } = auto; //Usamos destructuring para prevenir el usar auto.marca o auto.modelo
-        const autoHTML = document.createElement("p"); //Creamos un elemento "p" para cada item iterado
+        const autoHTML = document.createElement("p"); //Creamos un elemento "p" para cada item iterado, este codigo se repite para c/u
         autoHTML.innerHTML = `
             ${marca} - ${modelo} - ${year} - $${precio} - Puertas: ${puertas} - Color: ${color} - ${transmision};
         `;
-        resultado.appendChild(autoHTML);
+        resultado.appendChild(autoHTML); //Metemos los elementos de los carros en la variable resultado
     });
 };
+
+//Genera los años del select
+function llenarSelect() {
+    for (let i = max; i >= min; i--){
+        //max 2023, cuando i > min sea false, el bucle se detiene, max en cada recorrido quita 1 al max
+        const opcion = document.createElement("option");
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); //Agrega las opciones de año al select
+
+    }
+}
